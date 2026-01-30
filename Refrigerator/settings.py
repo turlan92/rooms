990dfd3bb205d94844cea0c02d071a21b20966fd
turlan92 <turlan92@gmail.com>
2026-01-30 16:15:41 +0500

    Deploy project to Railway

--- Refrigerator/settings.py
+++ Refrigerator/settings.py
@@ -69,11 +69,14 @@ WSGI_APPLICATION = 'Refrigerator.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': 'postgres.railway.internal',
+        'PORT': os.environ.get('PGPORT'),
+    }
 }
 
 # Password validation
